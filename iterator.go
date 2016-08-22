5bb8e1e1681d900c04909f338dc2fc15599cedf1
thbourlove <thbourlove@gmail.com>
2016-08-23 00:45:31 +0800

    implement FieldDimensions

--- iterator.go
+++ iterator.go
@@ -21,11 +21,77 @@ func NewIteratorCreator(clients []client.Client) (influxql.IteratorCreator, erro
 }
 
 type RemoteIteratorCreator struct {
-	client client.Client
+	client        client.Client
+	fieldsMap     map[string]map[string]influxql.DataType
+	dimensionsMap map[string]map[string]struct{}
 }
 
-func (ic *RemoteIteratorCreator) FieldDimensions(sources influxql.Sources) (fields map[string]influxql.DataType, dimensions map[string]struct{}, err error) {
-	return nil, nil, nil
+func (ic *RemoteIteratorCreator) FieldDimensions(sources influxql.Sources) (map[string]influxql.DataType, map[string]struct{}, error) {
+	if len(ic.fieldsMap) == 0 {
+		q := client.Query{
+			Command:  "show field keys",
+			Database: "esm",
+		}
+		resp, err := ic.client.Query(q)
+		if err != nil {
+			return nil, nil, fmt.Errorf("query %v: %v", q, err)
+		}
+		ic.fieldsMap = make(map[string]map[string]influxql.DataType, len(resp.Results[0].Series))
+		for _, row := range resp.Results[0].Series {
+			fields := map[string]influxql.DataType{}
+			for _, value := range row.Values {
+				fieldKey := value[0].(string)
+				fieldType := value[1].(string)
+				switch fieldType {
+				case "float":
+					fields[fieldKey] = influxql.Float
+				case "integer":
+					fields[fieldKey] = influxql.Integer
+				case "string":
+					fields[fieldKey] = influxql.String
+				case "bool":
+					fields[fieldKey] = influxql.Boolean
+				}
+			}
+			ic.fieldsMap[row.Name] = fields
+		}
+	}
+
+	if len(ic.dimensionsMap) == 0 {
+		q := client.Query{
+			Command:  "show tag keys",
+			Database: "esm",
+		}
+		resp, err := ic.client.Query(q)
+		if err != nil {
+			return nil, nil, fmt.Errorf("query %v: %v", q, err)
+		}
+		ic.dimensionsMap = make(map[string]map[string]struct{}, len(resp.Results[0].Series))
+		for _, row := range resp.Results[0].Series {
+			dimensions := map[string]struct{}{}
+			for _, value := range row.Values {
+				tagKey := value[0].(string)
+				dimensions[tagKey] = struct{}{}
+			}
+			ic.dimensionsMap[row.Name] = dimensions
+		}
+	}
+
+	fields := map[string]influxql.DataType{}
+	for _, name := range sources.Names() {
+		for fieldKey, fieldType := range ic.fieldsMap[name] {
+			fields[fieldKey] = fieldType
+		}
+	}
+
+	dimensions := map[string]struct{}{}
+	for _, name := range sources.Names() {
+		for tagKey, _ := range ic.dimensionsMap[name] {
+			dimensions[tagKey] = struct{}{}
+		}
+	}
+
+	return fields, dimensions, nil
 }
 
 func (ic *RemoteIteratorCreator) ExpandSources(sources influxql.Sources) (influxql.Sources, error) {
@@ -63,11 +129,7 @@ func (ic *RemoteIteratorCreator) CreateIterator(opt influxql.IteratorOptions) (i
 }
 
 func (ic *RemoteIteratorCreator) CreateVarRefIterator(options influxql.IteratorOptions) ([]influxql.Iterator, error) {
-	if _, ok := options.Expr.(*influxql.VarRef); !ok {
-		return nil, fmt.Errorf("expr is not varref")
-	}
 	stmt := &influxql.SelectStatement{
-		Fields:    []*influxql.Field{&influxql.Field{Expr: options.Expr}},
 		Sources:   options.Sources,
 		Condition: influxql.CloneExpr(options.Condition),
 		Limit:     options.Limit,
@@ -77,6 +139,21 @@ func (ic *RemoteIteratorCreator) CreateVarRefIterator(options influxql.IteratorO
 		Fill:      options.Fill,
 		FillValue: options.FillValue,
 	}
+
+	if len(options.Aux) > 0 {
+		//stmt.Fields = options.
+		stmt.Fields = []*influxql.Field{}
+		for _, ref := range options.Aux {
+			stmt.Fields = append(stmt.Fields, &influxql.Field{Expr: &ref})
+		}
+	} else {
+		if _, ok := options.Expr.(*influxql.VarRef); !ok {
+			return nil, fmt.Errorf("expr is not VarRef")
+		} else {
+			stmt.Fields = []*influxql.Field{&influxql.Field{Expr: options.Expr}}
+		}
+	}
+
 	for _, d := range options.Dimensions {
 		stmt.Dimensions = append(stmt.Dimensions, &influxql.Dimension{Expr: &influxql.VarRef{Val: d, Type: influxql.Tag}})
 	}
@@ -102,13 +179,28 @@ func (ic *RemoteIteratorCreator) CreateVarRefIterator(options influxql.IteratorO
 		points := make([]influxql.FloatPoint, 0, len(row.Values))
 		for _, value := range row.Values {
 			time, _ := strconv.ParseInt(string(value[0].(json.Number)), 10, 64)
-			v, _ := strconv.ParseFloat(string(value[1].(json.Number)), 64)
-			points = append(points, influxql.FloatPoint{
-				Name:  row.Columns[1],
-				Tags:  influxql.NewTags(row.Tags),
-				Time:  time * 1000000000,
-				Value: v,
-			})
+			if len(options.Aux) > 0 {
+				aux := []interface{}{}
+				for i := 1; i < len(value); i++ {
+					v, _ := strconv.ParseFloat(string(value[i].(json.Number)), 64)
+					aux = append(aux, v)
+				}
+				points = append(points, influxql.FloatPoint{
+					Name: row.Name,
+					//Name:  row.Columns[1],
+					Tags: influxql.NewTags(row.Tags),
+					Time: time * 1000000000,
+					Aux:  aux,
+				})
+			} else {
+				v, _ := strconv.ParseFloat(string(value[1].(json.Number)), 64)
+				points = append(points, influxql.FloatPoint{
+					Name:  row.Columns[1],
+					Tags:  influxql.NewTags(row.Tags),
+					Time:  time * 1000000000,
+					Value: v,
+				})
+			}
 		}
 		itrs = append(itrs, &FloatIterator{
 			points: points,
